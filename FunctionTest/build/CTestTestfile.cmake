272f6ddf505e420a42770f6c6fb32e1f82f191a8
Hao Lee <haolee0702@engineering.ucla.edu>
2021-09-23 13:06:19 -0700

    remove build folder

--- FunctionTest/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lowlimb/cdrive/UCLA/lab/Exoskeleton/Controller/ExoRPI4/FunctionTest
-# Build directory: /home/lowlimb/cdrive/UCLA/lab/Exoskeleton/Controller/ExoRPI4/FunctionTest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
