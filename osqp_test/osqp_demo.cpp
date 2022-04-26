3d3d783284612f8f21f35e4bd259725d6f929ae6
Hao Lee <haolee0702@engineering.ucla.edu>
2022-04-25 17:39:31 -0700

    Test osqp, manage to cross compiled, but required shared lib

---
+++ osqp_test/osqp_demo.cpp
@@ -0,0 +1,21 @@
+#include <osqp/osqp.h>
+#include "MPC.h"
+
+
+int main(int argc, char **argv) {
+    
+
+    MPC ins;
+    ins.Solve();
+    std::array<float,X_LEN> res1 = ins.GetResult();
+    for(const auto& e:res1){
+        std::cout<<e<<std::endl;
+    }
+    std::array<float,LIN_COST_LEN> new_q{1,2};
+    ins.UpdateLinCost(new_q);
+    ins.Solve();
+    std::array<float,X_LEN> res2 = ins.GetResult();
+    for(const auto& e:res2){
+        std::cout<<e<<std::endl;
+    }
+};
